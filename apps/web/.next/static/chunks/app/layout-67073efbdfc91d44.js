0a74f1e885dda555f0f88095ae4a30ec4408a15c
Caleb Wodi <calebwodi33@gmail.com>
2026-01-13 12:52:46 +0100

    chore: ignore next build output

--- apps/web/.next/static/chunks/app/layout-67073efbdfc91d44.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},2565:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1290,23))}},e=>{e.O(0,[741,441,255,358],()=>e(e.s=2565)),_N_E=e.O()}]);
